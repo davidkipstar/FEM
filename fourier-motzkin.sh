68a1b500c1e34c2b5d5cf9473d51100490a0c8c7
davidkipstar <david.arnold93@gmail.com>
2019-12-05 19:32:25 +0100

    bla bla

---
+++ fourier-motzkin.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+python3 main.py project './instances/project_test_1.dat' 2 './result_project.txt'
+python3 main.py image './instances/image_test_poly_1.dat' './instances/image_test_M_1.dat' './result_image.txt'
+python3 main.py H_representation './instances/H_representation_test_1.dat' './result_H_representation.txt'
+python3 main.py compute_x_or_y './instances/compute_x_or_y_test.dat'
+#
+#python3 main.py  "$1" "$2" "$3" "$4" 
